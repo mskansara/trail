6b43e482608f5ade5bf6f5283414b5d5c453bff7
Manthan Kansara <manthankansara7@gmail.com>
2026-07-24 22:02:04 +0100

    Project skeleton: parent POM, incident service module, local infra

---
+++ backend/trail-incident-service/src/main/java/io/trail/incident/TrailIncidentServiceApplication.java
@@ -0,0 +1,13 @@
+package io.trail.incident;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailIncidentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrailIncidentServiceApplication.class, args);
+	}
+
+}
